559504b53d40e16ea4137c2b196bce180cb90a96
George <georgeabush1@gmail.com>
2021-06-04 17:06:40 -0400

    FINALLY FIXED PAGES, HAD TO FIX PACKAGE.JSON

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/JEG-Website/precache-manifest.715a1d076184682c30bc3b3693305a5f.js"
+  "/JEG-Website/precache-manifest.6023756d94afecc73d7638b1dad2e40c.js"
 );
 
 self.addEventListener('message', (event) => {
